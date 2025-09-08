95e1e51cc6e32c69831528f2ff80d20bb98f9ab7
tconuorah <terrenceonuorah@gmail.com>
2025-09-07 20:17:12 -0500

    Updated frontend and backend config.js code to utilize the ALB DNS

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    CORS_ORIGIN: 'http://devops-challenge-alb-1209999776.us-east-2.elb.amazonaws.com'
+    CORS_ORIGIN: 'https://devops-challenge-alb-1209999776.us-east-2.elb.amazonaws.com'
 };
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-1209999776.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "https://devops-challenge-alb-1209999776.us-east-2.elb.amazonaws.com/api"
 };
 export default config;
